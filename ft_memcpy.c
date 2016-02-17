e7f6cdac48bbc0f80cb83ff164de4adbdfb2df8c
Ryad KHARIF <rkharif@e2r12p18.42.fr>
2016-02-17 14:40:26 +0100

    First functions

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkharif <rkharif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/17 14:32:23 by rkharif           #+#    #+#             */
+/*   Updated: 2016/02/17 14:37:52 by rkharif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*strd;
+	const char	*strs;
+
+	strd = dst;
+	strs = src;
+	while (n-- > 0)
+	{
+		
+	}
+
+	return (dst);
+}
